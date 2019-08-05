0afd4d59dedd175ed99cf3edd88b2c0c887b0897
Salwa Fathallah <salwafathallah@Salwas-MacBook-Pro.local>
2019-08-05 15:32:09 +0100

    add google analytics

--- src/components/layout.js
+++ src/components/layout.js
@@ -33,15 +33,6 @@ const Layout = ({ children }) => {
           rel="stylesheet"
           href="https://fonts.googleapis.com/css?family=Rubik"
         ></link>
-        <script
-          async
-          src="https://www.googletagmanager.com/gtag/js?id=UA-144983266-1"
-        ></script>
-        <script>
-          window.dataLayer = window.dataLayer || []; function gtag()
-          {dataLayer.push(arguments)}
-          gtag('js', new Date()); gtag('config', 'UA-144983266-1');
-        </script>
       </head>
       <main>{children}</main>
     </>
