ab42396a730eebcf278f8ea89136967468c1915b
Salwa Fathallah <salwafathallah@Salwas-MacBook-Pro.local>
2019-08-05 15:37:59 +0100

    add google analytics

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,18 @@
 import React from "react"
+import ReactGA from "react-ga"
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 import Navigation from "../components/navigation"
 import Me from "../components/me"
 
+function initializeReactGA() {
+  ReactGA.initialize("UA-144983266-1")
+  ReactGA.pageview("/")
+}
 class IndexPage extends React.Component {
   render() {
+    initializeReactGA()
     return (
       <Layout>
         <SEO title="Salwa Fathallah" />
