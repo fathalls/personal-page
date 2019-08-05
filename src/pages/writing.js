ff19c6cb6f7cc183c6b273059c6b5513536bc6d3
Salwa Fathallah <salwafathallah@Salwas-MacBook-Pro.local>
2019-08-05 12:42:12 +0100

    change SEO to full name

--- src/pages/writing.js
+++ src/pages/writing.js
@@ -9,7 +9,7 @@ class Writing extends React.Component {
   render() {
     return (
       <Layout>
-        <SEO title="Salwa F." />
+        <SEO title="Salwa Fathallah" />
         <div className="row">
           <Navigation />
           <div class="content-column">
