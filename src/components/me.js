6f4d6e1437b8086d07a3c3d841498ad6066ab3dd
Salwa Fathallah <salwafathallah@Salwas-MacBook-Pro.local>
2019-08-09 16:28:46 +0100

    add contact info in bio

--- src/components/me.js
+++ src/components/me.js
@@ -12,6 +12,7 @@ const Me = () => {
                             href="https://www.swyx.io/writing/learn-in-public/" target="_blank">learning in public</a>.
                     </p>
                     <p>This site refrences my learnings & some of the projects I worked on in my free time.</p>
+                    <p>You can find me on <a href="https://twitter.com/salwa_fathallah" target="_blank">Twitter</a>, have a look at my work experience in <a href="https://www.linkedin.com/in/salwa-fathallah-74a07232/"  target="_blank">LinkedIn</a>, or check what I'm up to in <a href="https://github.com/fathalls"  target="_blank">GitHub</a>.</p>
                 </div>
             </div>
         </div>
