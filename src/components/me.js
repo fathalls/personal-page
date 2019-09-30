6dbc5f4d95944fd1abadeb87a0f279440d0f2cb0
Salwa Fathallah <salwafathallah@Salwas-MacBook-Pro.local>
2019-09-30 18:51:10 +0100

    change bio

--- src/components/me.js
+++ src/components/me.js
@@ -7,12 +7,12 @@ const Me = () => {
                 <div className="bio-description">
                     <h1>Salwa Fathallah</h1>
                     <p>Software engineer based in London. I like minimalism and building things from scratch.</p>
-                    <p>I believe in <a className="compassionate-coding" href="https://compassionatecoding.com/"
+                    {/* <p>I believe in <a className="compassionate-coding" href="https://compassionatecoding.com/"
                             target="_blank">Compassionate Coding</a> and I practice <a className="learn-in-public"
                             href="https://www.swyx.io/writing/learn-in-public/" target="_blank">learning in public</a>.
-                    </p>
-                    <p>This site refrences my learnings & some of the projects I worked on in my free time.</p>
-                    <p>You can find me on <a href="https://twitter.com/salwa_fathallah" target="_blank">Twitter</a>, have a look at my work experience in <a href="https://www.linkedin.com/in/salwa-fathallah-74a07232/"  target="_blank">LinkedIn</a>, or check what I'm up to in <a href="https://github.com/fathalls"  target="_blank">GitHub</a>.</p>
+                    </p> */}
+                    <p>This site references some of my thoughts, some of my continuous learnings & a couple of projects I worked on in my free time.</p>
+                    <p>You can find me on <a href="https://twitter.com/salwa_fathallah" target="_blank">Twitter</a>, have a look at my work experience on <a href="https://www.linkedin.com/in/salwa-fathallah-74a07232/"  target="_blank">LinkedIn</a>, or check what I'm up to in <a href="https://github.com/fathalls"  target="_blank">GitHub</a>.</p>
                 </div>
             </div>
         </div>
